33da0a899e68fff839c6c9b4cff5e346fd179dec
krishal2003 <120012179+krishal2003@users.noreply.github.com>
2025-07-14 10:27:24 +0545

    Update page.tsx

--- app/career/page.tsx
+++ app/career/page.tsx
@@ -86,7 +86,7 @@ export default function CareersPage() {
       <section className="py-20">
         <div className="container mx-auto px-4">
           <div className="text-center mb-16">
-            <h2 className="text-4xl font-bold text-foreground mb-4">
+            <h2 className=" mt-8 text-4xl font-bold text-foreground mb-4">
               Open Positions
             </h2>
             <p className="text-xl text-muted-foreground max-w-2xl mx-auto">
