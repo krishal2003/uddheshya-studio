3459b497ba1675408875a05ce0a9a201206ecdc7
krishal2003 <120012179+krishal2003@users.noreply.github.com>
2025-07-14 07:36:45 +0545

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,19 @@
-import HeroSection from "@/components/hero-section"
-import FeaturedServices from "@/components/featured-services"
-import ClientsSection from "@/components/clients-section"
-import TestimonialsSection from "@/components/testimonials-section"
-import CtaSection from "@/components/cta-section"
+import HeroSection from "@/components/hero-section";
+import FeaturedServices from "@/components/featured-services";
+import ClientsSection from "@/components/clients-section";
+import TestimonialsSection from "@/components/testimonials-section";
+import CtaSection from "@/components/cta-section";
+import VideoSection from "@/components/VideoSection";
 
 export default function Home() {
   return (
     <div className="overflow-hidden">
+      <VideoSection />
       <HeroSection />
       <FeaturedServices />
-      <ClientsSection />
+      {/* <ClientsSection /> */}
       <TestimonialsSection />
       <CtaSection />
     </div>
-  )
+  );
 }
