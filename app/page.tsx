9bb7c76d41d6f9ea296363c67ec33eae6ae0ffa5
krishal2003 <120012179+krishal2003@users.noreply.github.com>
2025-08-20 12:52:32 +0545

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,11 @@ import VideoSection from "@/components/VideoSection";
 export default function Home() {
   return (
     <div className="overflow-hidden">
-      <VideoSection />
+      {/* Video section hidden on mobile, visible on medium and larger screens */}
+      <div className="hidden md:block">
+        <VideoSection />
+      </div>
+
       <HeroSection />
       <FeaturedServices />
       {/* <ClientsSection /> */}
