8db9378aa1eb8f87459ed9a4a728811e11225b3a
krishal2003 <120012179+krishal2003@users.noreply.github.com>
2025-09-06 10:15:49 +0545

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,4 @@
-import HeroSection from "@/components/hero-section";
-import FeaturedServices from "@/components/featured-services";
-import ClientsSection from "@/components/clients-section";
-import TestimonialsSection from "@/components/testimonials-section";
-import CtaSection from "@/components/cta-section";
-import VideoSection from "@/components/VideoSection";
+
 
 export default function Home() {
   return (
